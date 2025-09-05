2e609b4ce9f5396d4ecc32268f8007b505d99ffc
Mateusz Bocian <mrstork@users.noreply.github.com>
2025-09-05 18:56:20 -0400

    Set a consistent build id to minimize differences between builds

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+  generateBuildId: () => "5AorX1qMF0qfE6BCd-SX6",
+  output: "standalone",
 };
 
 export default nextConfig;
